dd11ced97d77d24adaf4bddd6ac460cbf60a069c
Srinatha Prabhu <srinatha.prabhu@Srinathas-MacBook-Pro.local>
2020-10-22 09:32:33 +0530

    <Sri>fixing some errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @person name
+   * @return greeting string
+   */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
